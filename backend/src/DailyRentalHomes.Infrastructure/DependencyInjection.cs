3c3d8aeec65c570c3c0ce7e4cba43516e6ec9e26
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 15:47:37 +0400

    Add infrastructure dependency injection

---
+++ backend/src/DailyRentalHomes.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using DailyRentalHomes.Application.Abstractions.Persistence;
+using DailyRentalHomes.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyRentalHomes.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+
+        return services;
+    }
+}
