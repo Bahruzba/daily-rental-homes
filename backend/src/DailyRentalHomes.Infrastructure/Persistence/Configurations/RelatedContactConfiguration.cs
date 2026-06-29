862d3f15ce15783e2d26b98b4a9d4175b800f2c9
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 15:48:20 +0400

    Add related contact EF configuration

---
+++ backend/src/DailyRentalHomes.Infrastructure/Persistence/Configurations/RelatedContactConfiguration.cs
@@ -0,0 +1,18 @@
+using DailyRentalHomes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyRentalHomes.Infrastructure.Persistence.Configurations;
+
+public sealed class RelatedContactConfiguration : IEntityTypeConfiguration<RelatedContact>
+{
+    public void Configure(EntityTypeBuilder<RelatedContact> builder)
+    {
+        builder.ToTable("related_contacts");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.FullName).HasMaxLength(150).IsRequired();
+        builder.Property(x => x.Value).HasMaxLength(100).IsRequired();
+        builder.HasIndex(x => x.RentalHomeId);
+        builder.HasIndex(x => x.ContactType);
+    }
+}
