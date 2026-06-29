38b8b45bc2231791165f9ea5e53410301b6960a7
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 15:42:06 +0400

    Add app db context abstraction

---
+++ backend/src/DailyRentalHomes.Application/Abstractions/Persistence/IAppDbContext.cs
@@ -0,0 +1,17 @@
+using DailyRentalHomes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyRentalHomes.Application.Abstractions.Persistence;
+
+public interface IAppDbContext
+{
+    DbSet<User> Users { get; }
+    DbSet<RentalHome> RentalHomes { get; }
+    DbSet<Booking> Bookings { get; }
+    DbSet<BookingDate> BookingDates { get; }
+    DbSet<BookingDeposit> BookingDeposits { get; }
+    DbSet<BookingStatus> BookingStatuses { get; }
+    DbSet<PaymentCard> PaymentCards { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
