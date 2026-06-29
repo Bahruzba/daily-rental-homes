532835043e2bd54d3ddeae1336c2571d5126b2a2
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 15:47:46 +0400

    Add rental home EF configuration

---
+++ backend/src/DailyRentalHomes.Infrastructure/Persistence/Configurations/RentalHomeConfiguration.cs
@@ -0,0 +1,22 @@
+using DailyRentalHomes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyRentalHomes.Infrastructure.Persistence.Configurations;
+
+public sealed class RentalHomeConfiguration : IEntityTypeConfiguration<RentalHome>
+{
+    public void Configure(EntityTypeBuilder<RentalHome> builder)
+    {
+        builder.ToTable("rental_homes");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Title).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.Description).HasMaxLength(4000);
+        builder.Property(x => x.City).HasMaxLength(100).IsRequired();
+        builder.Property(x => x.District).HasMaxLength(100);
+        builder.Property(x => x.Address).HasMaxLength(500);
+        builder.Property(x => x.DailyPrice).HasPrecision(18, 2);
+        builder.HasIndex(x => x.City);
+        builder.HasIndex(x => x.BrokerUserId);
+    }
+}
