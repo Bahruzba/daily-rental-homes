553591c84f46d3f81cfc04a6ec37f6a3df8b5b4d
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 15:48:00 +0400

    Add booking date EF configuration

---
+++ backend/src/DailyRentalHomes.Infrastructure/Persistence/Configurations/BookingDateConfiguration.cs
@@ -0,0 +1,16 @@
+using DailyRentalHomes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyRentalHomes.Infrastructure.Persistence.Configurations;
+
+public sealed class BookingDateConfiguration : IEntityTypeConfiguration<BookingDate>
+{
+    public void Configure(EntityTypeBuilder<BookingDate> builder)
+    {
+        builder.ToTable("booking_dates");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Date).IsRequired();
+        builder.HasIndex(x => new { x.BookingId, x.Date }).IsUnique();
+    }
+}
