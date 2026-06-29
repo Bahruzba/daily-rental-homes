fd40e38becafe0a71d8bdf34f176bc88e0ee1aa7
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 15:37:36 +0400

    Add booking deposit status enum

---
+++ backend/src/DailyRentalHomes.Domain/Enums/BookingDepositStatus.cs
@@ -0,0 +1,12 @@
+namespace DailyRentalHomes.Domain.Enums;
+
+public enum BookingDepositStatus
+{
+    NotRequired = 1,
+    Waiting = 2,
+    Paid = 3,
+    Expired = 4,
+    Returned = 5,
+    NotReturned = 6,
+    Cancelled = 7
+}
