9555d8b1e22e4b773a029b04d900c5ff750d9c08
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 15:48:15 +0400

    Add media file EF configuration

---
+++ backend/src/DailyRentalHomes.Infrastructure/Persistence/Configurations/MediaFileConfiguration.cs
@@ -0,0 +1,20 @@
+using DailyRentalHomes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyRentalHomes.Infrastructure.Persistence.Configurations;
+
+public sealed class MediaFileConfiguration : IEntityTypeConfiguration<MediaFile>
+{
+    public void Configure(EntityTypeBuilder<MediaFile> builder)
+    {
+        builder.ToTable("media_files");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.FileName).HasMaxLength(255).IsRequired();
+        builder.Property(x => x.FileUrl).HasMaxLength(1000).IsRequired();
+        builder.Property(x => x.ContentType).HasMaxLength(100);
+        builder.HasIndex(x => x.RentalHomeId);
+        builder.HasIndex(x => x.BookingDepositId);
+        builder.HasIndex(x => x.FileType);
+    }
+}
