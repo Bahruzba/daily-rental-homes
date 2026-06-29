6a4c85ef619305798b467ddefd822c58197cb8f6
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 16:03:54 +0400

    Add media and contact sets to app context abstraction

--- backend/src/DailyRentalHomes.Application/Abstractions/Persistence/IAppDbContext.cs
+++ backend/src/DailyRentalHomes.Application/Abstractions/Persistence/IAppDbContext.cs
@@ -7,6 +7,8 @@ public interface IAppDbContext
 {
     DbSet<User> Users { get; }
     DbSet<RentalHome> RentalHomes { get; }
+    DbSet<MediaFile> MediaFiles { get; }
+    DbSet<RelatedContact> RelatedContacts { get; }
     DbSet<Booking> Bookings { get; }
     DbSet<BookingDate> BookingDates { get; }
     DbSet<BookingDeposit> BookingDeposits { get; }
