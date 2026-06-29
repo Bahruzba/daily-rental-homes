f816a350d1b71eb1e5807971b669d8f20cab301f
Bahruz Ahmadov <47360893+Bahruzba@users.noreply.github.com>
2026-06-29 16:33:48 +0400

    Add outbound message EF configuration

---
+++ backend/src/DailyRentalHomes.Infrastructure/Persistence/Configurations/OutboundMessageConfiguration.cs
@@ -0,0 +1,22 @@
+using DailyRentalHomes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyRentalHomes.Infrastructure.Persistence.Configurations;
+
+public sealed class OutboundMessageConfiguration : IEntityTypeConfiguration<OutboundMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboundMessage> builder)
+    {
+        builder.ToTable("outbound_messages");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.To).HasMaxLength(100).IsRequired();
+        builder.Property(x => x.Text).HasMaxLength(2000).IsRequired();
+        builder.Property(x => x.ProviderMessageId).HasMaxLength(200);
+        builder.Property(x => x.ErrorMessage).HasMaxLength(1000);
+        builder.HasIndex(x => x.Channel);
+        builder.HasIndex(x => x.Status);
+        builder.HasIndex(x => x.BookingId);
+        builder.HasIndex(x => x.BookingDepositId);
+    }
+}
